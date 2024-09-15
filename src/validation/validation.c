7572f177b6e22db47586b534eb074672433807c8
YuriFont <yurifontenelesilva180303@gmail.com>
2024-09-15 12:34:15 -0300

    :sparkles: feat: implements test of return in stderr

--- src/validation/validation.c
+++ src/validation/validation.c
@@ -19,7 +19,7 @@ static void	validate_extension(char *file)
 	i = ft_strlen(file);
 	if (i < 5 || file[i - 1] != 'b' || file[i - 2] != 'u'
 		|| file[i - 3] != 'c' || file[i - 4] != '.')
-		error("Error: the file must have a .cub extension.");
+		error("Error: the file must have a .cub extension");
 }
 
 void	validation(char *file)
--- tests.sh
+++ tests.sh
@@ -1,29 +1,45 @@
 #!/bin/bash
 
 ./cub3D 2> stderr_output
+cr_cub=$?
 
-if [ $? -ne 1 ]; then
+grep -q "Error: cub3D only accepts one parameter" stderr_output
+cr_grep=$?
+
+if [ $cr_cub -ne 1 ] || [ $cr_grep -ne 0 ]; then
     echo "Error in test 01: ./cub3D"
     exit 1
 fi
 
 ./cub3D test test 2> stderr_output
+cr_cub=$?
 
-if [ $? -ne 1 ]; then
+grep -q "Error: cub3D only accepts one parameter" stderr_output
+cr_grep=$?
+
+if [ $cr_cub -ne 1 ] || [ $cr_grep -ne 0 ]; then
     echo "Error in test 02: ./cubeD test test"
     exit 1
 fi
 
 ./cub3D test 2> stderr_output
+cr_cub=$?
 
-if [ $? -ne 1 ]; then
+grep -q "Error: the file must have a .cub extension" stderr_output
+cr_grep=$?
+
+if [ $cr_cub -ne 1 ] || [ $cr_grep -ne 0 ]; then
     echo "Error in test 03: ./cubeD test"
     exit 1
 fi
 
 ./cub3D .cub 2> stderr_output
+cr_cub=$?
 
-if [ $? -ne 1 ]; then
+grep -q "Error: the file must have a .cub extension" stderr_output
+cr_grep=$?
+
+if [ $cr_cub -ne 1 ] || [ $cr_grep -ne 0 ]; then
     echo "Error in test 04: ./cubeD .cub"
     exit 1
 fi
